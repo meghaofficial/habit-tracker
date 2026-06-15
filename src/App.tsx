d761ac3f5f7708b48076c1e564626d9679b06125
meghasharma <meghaofficial789@gmail.com>
2026-06-15 23:13:36 +0530

    no internet conn

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import PageLoader from "./components/loaders/PageLoader";
 import { ToastContainer } from "react-toastify";
 import Settings from "./components/pages/Settings";
 import { socket } from "./socket/socket";
+import NoInternetConnection from "./components/shared/NoInternetConnection";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ function App() {
   const [isAuthLoading, setIsAuthLoading] = useState(false);
   const user = useSelector((state: RootState) => state.auth);
   const theme = useSelector((state: RootState) => state.theme);
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
 
   useEffect(() => {
     const root = window.document.documentElement;
@@ -69,48 +71,67 @@ function App() {
     };
   }, [user?.id]);
 
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true);
+    const handleOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
   return (
     <>
-      <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden -z-10 opacity-[0.6] blur-[80px]">
-        <div
-          className="absolute rounded-1/2 -top-[25%] -left-[15%] w-[50vw] h-[50vh]"
-          style={{
-            background: `radial-gradient(circle, #6365f1d4 0%, rgba(0,0,0,0) 70%)`,
-            animation: `floatAround 25s infinite alternate ease-in-out`,
-            mixBlendMode: "screen",
-          }}
-        ></div>
-        <div
-          className="absolute rounded-1/2 -top-[15%] -right-[15%] w-[45vw] h-[45vh]"
-          style={{
-            background: `radial-gradient(circle, #a955f77f 0%, rgba(0,0,0,0) 70%)`,
-            animation: `floatAround 30s infinite alternate-reverse ease-in-out`,
-            mixBlendMode: "screen",
-          }}
-        ></div>
-      </div>
-      <div className="main-content light:bg-[#F5F5F6]">
-        {isAuthLoading ? (
-          <div className="flex items-center justify-center h-screen">
-            <PageLoader />
+      {isOnline ? (
+        <NoInternetConnection />
+      ) : (
+        <>
+          <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden -z-10 opacity-[0.6] blur-[80px]">
+            <div
+              className="absolute rounded-1/2 -top-[25%] -left-[15%] w-[50vw] h-[50vh]"
+              style={{
+                background: `radial-gradient(circle, #6365f1d4 0%, rgba(0,0,0,0) 70%)`,
+                animation: `floatAround 25s infinite alternate ease-in-out`,
+                mixBlendMode: "screen",
+              }}
+            ></div>
+            <div
+              className="absolute rounded-1/2 -top-[15%] -right-[15%] w-[45vw] h-[45vh]"
+              style={{
+                background: `radial-gradient(circle, #a955f77f 0%, rgba(0,0,0,0) 70%)`,
+                animation: `floatAround 30s infinite alternate-reverse ease-in-out`,
+                mixBlendMode: "screen",
+              }}
+            ></div>
           </div>
-        ) : (
-          <div className="text-darkText light:text-lightText">
-            <ToastContainer position="top-right" autoClose={3000} />
-            <Routes>
-              <Route
-                path="/"
-                element={isLogin ? <Dashboard /> : <HomePage />}
-              />
-              <Route path="/demo" element={<Demo />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/*" element={<PageNotFound />} />
-            </Routes>
-          </div>
-        )}
+          <div className="main-content light:bg-[#F5F5F6]">
+            {isAuthLoading ? (
+              <div className="flex items-center justify-center h-screen">
+                <PageLoader />
+              </div>
+            ) : (
+              <div className="text-darkText light:text-lightText">
+                <ToastContainer position="top-right" autoClose={3000} />
+                <Routes>
+                  <Route
+                    path="/"
+                    element={isLogin ? <Dashboard /> : <HomePage />}
+                  />
+                  <Route path="/demo" element={<Demo />} />
+                  <Route path="/settings" element={<Settings />} />
+                  <Route path="/*" element={<PageNotFound />} />
+                </Routes>
+              </div>
+            )}
 
-        {/* <FloatingActionButtonComp /> */}
-      </div>
+            {/* <FloatingActionButtonComp /> */}
+          </div>
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/shared/NoInternetConnection.tsx
@@ -0,0 +1,9 @@
+const NoInternetConnection = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <p className="text-[50px] font-bold google-sans">Please check your Internet Connection!</p>
+    </div>
+  )
+}
+
+export default NoInternetConnection
