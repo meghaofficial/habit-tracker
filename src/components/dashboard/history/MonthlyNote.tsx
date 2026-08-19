50da8086a595e9e66ade26df6427d56ab812a724
meghasharma <meghaofficial789@gmail.com>
2026-08-19 13:45:00 +0530

    added loaders

--- src/components/dashboard/history/MonthlyNote.tsx
+++ src/components/dashboard/history/MonthlyNote.tsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import { axiosPrivate } from "../../../api/axios";
 import Card from "../../shared/Card";
 import { FiAward, FiFileText } from "react-icons/fi";
+import { formatTimestamp } from "../../../helper";
 
 const MonthlyNote = ({ monthDashID }: { monthDashID: string }) => {
+  const [loading, setLoading] = useState(false);
   const [note, setNote] = useState("");
+  const [lastUpdate, setLastUpdate] = useState("");
 
   const getNote = async () => {
+    setLoading(true);
     try {
       const res = await axiosPrivate.get(
         `/api/monthly-note?monthDashID=${monthDashID}`,
@@ -15,11 +19,13 @@ const MonthlyNote = ({ monthDashID }: { monthDashID: string }) => {
       if (res?.data?.success) {
         if (res?.data?.note) {
           setNote(res?.data?.note?.note);
-          console.log("res", res?.data);
+          setLastUpdate(res?.data?.note?.updatedAt);
         }
       }
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -35,18 +41,32 @@ const MonthlyNote = ({ monthDashID }: { monthDashID: string }) => {
         <h3 className="text-xs font-bold text-gray-400 uppercase tracking-widest flex items-center gap-2">
           <FiFileText className="text-orange-400" /> Reflections & Notes
         </h3>
-        <div className="bg-white/2 border border-white/5 rounded-2xl p-4 h-50 flex flex-col justify-between">
-          <p className="text-sm text-gray-300 leading-relaxed italic">{note}</p>
-          <div className="flex justify-between items-center text-[10px] text-gray-500 mt-4 border-t border-white/5 pt-3">
-            <span className="font-semibold flex items-center gap-1">
-              <FiAward className="text-yellow-500" />
-              Month Outcome
-            </span>
-            <span className="font-bold text-emerald-400 bg-emerald-500/10 border border-emerald-500/20 px-2 py-0.5 rounded">
-              COMPLETED
-            </span>
-          </div>
-        </div>
+        {loading ? (
+          <div className="h-50 overflow-y-hidden flex justify-center animate-pulse bg-white/5 rounded-2xl"></div>
+        ) : (
+          <>
+            {note ? (
+              <div className="bg-white/2 border border-white/5 rounded-2xl p-4 h-50 flex flex-col justify-between">
+                <p className="text-sm text-gray-300 leading-relaxed italic">
+                  {note}
+                </p>
+                <div className="flex justify-between items-center text-[10px] text-gray-500 mt-4 border-t border-white/5 pt-3">
+                  <span className="font-semibold flex items-center gap-1">
+                    <FiAward className="text-yellow-500" />
+                    Last updated at
+                  </span>
+                  <span className="font-bold text-emerald-400 bg-emerald-500/10 border border-emerald-500/20 px-2 py-0.5 rounded">
+                    {formatTimestamp(lastUpdate)}
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <div className="h-50 items-center flex justify-center">
+                <span className="text-[12px] text-gray-500">Not found!</span>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </Card>
   );
--- src/components/dashboard/history/MonthlyTargets.tsx
+++ src/components/dashboard/history/MonthlyTargets.tsx
@@ -40,21 +40,40 @@ const MonthlyTargets = ({ monthDashID }: { monthDashID: string }) => {
         <h3 className="text-xs font-bold text-gray-400 uppercase tracking-widest flex items-center gap-2">
           <FiTarget className="text-yellow-400" /> Monthly Targets Set
         </h3>
-        <ul className="space-y-3.5 mt-1 h-50 overflow-y-auto">
-          {monthlyTargets.map((t, index) => (
-            <li
-              key={index}
-              className={`flex items-start gap-3 border ${t.completed ? 'border-green-600/50 bg-green-600/5 hover:bg-green-600/10' : 'border-white/5 bg-white/1 hover:bg-white/3'}  p-3 rounded-xl transition-colors`}
-            >
-              <span className="flex items-center justify-center w-5 h-5 rounded-full bg-yellow-500/10 text-yellow-500 text-xs font-bold border border-yellow-500/20 shrink-0">
-                {index + 1}
-              </span>
-              <span className="text-sm font-medium text-gray-300 leading-tight">
-                {t.value}
-              </span>
-            </li>
-          ))}
-        </ul>
+        {loading ? (
+          <div className="h-50 overflow-y-hidden flex justify-center flex-col gap-3">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <div
+                key={index}
+                className="animate-pulse bg-white/5 h-10 w-full rounded-lg"
+              ></div>
+            ))}
+          </div>
+        ) : (
+          <>
+            {monthlyTargets.length > 0 ? (
+              <ul className="space-y-3.5 mt-1 h-50 overflow-y-auto">
+                {monthlyTargets.map((t, index) => (
+                  <li
+                    key={index}
+                    className={`flex items-start gap-3 border ${t.completed ? "border-green-600/50 bg-green-600/5 hover:bg-green-600/10" : "border-white/5 bg-white/1 hover:bg-white/3"}  p-3 rounded-xl transition-colors`}
+                  >
+                    <span className="flex items-center justify-center w-5 h-5 rounded-full bg-yellow-500/10 text-yellow-500 text-xs font-bold border border-yellow-500/20 shrink-0">
+                      {index + 1}
+                    </span>
+                    <span className="text-sm font-medium text-gray-300 leading-tight">
+                      {t.value}
+                    </span>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="h-50 items-center flex justify-center">
+                <span className="text-[12px] text-gray-500">Not found!</span>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </Card>
   );
